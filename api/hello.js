e63aafc3b32fa1d851d80bbdffd9fca328819b72
hafizhuzaifaahmed <huzaifaahmed90000@gmail.com>
2025-05-15 12:28:18 +0500

    Initial commit

---
+++ api/hello.js
@@ -0,0 +1,5 @@
+export default function handler(req, res) {
+  setTimeout(() => {
+    res.status(200).send("Hello from Vercel! Auto-scaling triggered.");
+  }, 1000); // Simulate 1s processing time
+}
